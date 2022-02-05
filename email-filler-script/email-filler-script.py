e90b523490b0a16a0cfca6f13afced564f8f9b88
Ayush Goyal <ayushg3@illinois.edu>
2022-02-05 17:00:12 -0600

    unnecessary_comments_removed_and_code_optimized

--- email-filler-script/email-filler-script.py
+++ email-filler-script/email-filler-script.py
@@ -2,93 +2,95 @@ import json
 import re
 import csv 
 
-userData=[]
 
-with open('user_data.csv', newline='') as dataInputFile:
-    userData = list( csv.DictReader(dataInputFile) )
+class EmailFiller :
+    userData = []
+    noOfColumns = 0
+    emailInput = {}
+    emailContent = ""
+    errorFlag = 0
 
-noOfColumns = len(userData[0])
-for i in range(noOfColumns):
-    ithColumnName = list(userData[0].keys())[i] 
-    userData[0][ithColumnName.lower()] = userData[0].pop(ithColumnName)
+    def readUserData(self, filename) :
+        """ Reads the user email data from the 'filename' csv file"""
+        with open(filename, newline='') as dataInputFile:
+            self.userData = list( csv.DictReader(dataInputFile) )
 
-# textInputFile = open('email_file.json', 'r+')  
-# emailInput = textInputFile.load() # Refine Input based on email and add files 
+        self.noOfColumns = len(self.userData[0])
 
-emailInput = {}
-with open('email_info.csv', newline='') as emailDataInputFile:
-    emailInput = dict( list(csv.DictReader(emailDataInputFile))[0] )
+        for i in range(self.noOfColumns):
+            ithColumnName = list(self.userData[0].keys())[0] 
+            self.userData[0][ithColumnName.lower()] = self.userData[0].pop(ithColumnName)
+
+    def readEmailInfo(self,filename):
+        """ Reads the email destination data from the 'filename' csv file"""
+        with open(filename, newline='') as emailDataInputFile:
+            self.emailInput = dict( list(csv.DictReader(emailDataInputFile))[0] )
+
+    def readEmailSubjectAndBody(self,filename) :
+        """ Reads the user email subject and body data from the 'filename' text file"""
+        with open( filename, newline='') as templateFile :
+            self.emailContent = templateFile.read()
+        subject = re.search(r'Subject: (.*)\n', self.emailContent,re.IGNORECASE )
+        emailBody = re.search(r'Subject: .*\n\n((.|\n)*)', self.emailContent,re.IGNORECASE )
+        self.emailInput['Subject'] = subject.group(1)
+        self.emailInput['body'] = emailBody.group(1) 
+
+    def userInputPattern(self) :
+        """ Takes and extract the pattern used for future data replacement in the email templates from user input.
+            We are taking the user input for the pattern becuase the encoding of auto detection of a pattern by the
+            script may be inefficienct since it may detect an incorrect pattern as the original pattern for replacement.
+            It is difficult to predict which combination of characters user might want to consider as the replacement 
+            pattern for the email template. 
+        """
+        patternString = input("Enter replace pattern: ")
+        pattern = re.search(r'([^a-zA-Z0-9]*)[a-zA-Z\s]+(.*)', patternString )
+        while pattern == None or pattern.group(1) == None or pattern.group(1) == '' or pattern.group(2) == None or pattern.group(2) == '':
+            patternString = input("Enter replace pattern again: ")
+            pattern = re.search(r'([^a-zA-Z0-9]*)[a-zA-Z\s]+(.*)', patternString , re.IGNORECASE)
+        return pattern 
+
+    def fillTemplate(self, pattern) :
+        """ Fills the user email tempalte with the user data obtained from csv files"""
+        replacedOutput = self.emailInput['body']
+        patternPrefix = re.sub(r'([\.\^\$\*\+\-\?\(\)\[\]\{\}\\\|\—\/])', r'\\\1', pattern.group(1) )   # List of all the special meaning characters in regular expression module
+        patternPostfix = re.sub(r'([\.\^\$\*\+\-\?\(\)\[\]\{\}\\\|\—\/])', r'\\\1', pattern.group(2) ) 
+        replacedOutput = re.sub(r'' + patternPrefix + '(.+)'+ patternPostfix, lambda m: self.userData[0].get(m.group(1).lower()) , replacedOutput ,flags=re.IGNORECASE )
+        self.emailInput['body'] = replacedOutput
+
+    def errorDetection(self, pattern) :
+        """ Detects errors based on the user specified pattern in the email template"""
+        for i in range(self.noOfColumns):
+            searchResult = re.search('([^\sa-zA-Z]+)' + list(self.userData[0].keys())[i] + '([^\sa-zA-Z]+)', self.emailInput['body'], re.IGNORECASE)
+            if searchResult ==  None:
+                continue 
+            if searchResult.group(1) != pattern.group(1) or searchResult.group(2) != pattern.group(2) :
+                self.errorFlag = 1
+        if self.errorFlag == 1 :
+                print("There are errors in the drafted Email")
+        else:
+                print("The drafted email is error free")
+
+    def writeJsonFile(self) :
+        """ Writes the collaborated details regarding the user email onto a json file"""
+        jsonOutput = json.dumps(self.emailInput)
+        textInputFile = open('email_formatted.json', 'r+')   
+        json.dump(jsonOutput,textInputFile)
+        print("JSON File Updated")
+        textInputFile.close()
+
+
+fillUserEmail = EmailFiller()
+fillUserEmail.readEmailInfo('email_info.csv')
+fillUserEmail.readEmailSubjectAndBody('email_template.txt')
+fillUserEmail.readUserData('user_data.csv')
+pattern = fillUserEmail.userInputPattern()
+fillUserEmail.fillTemplate(pattern)
+fillUserEmail.errorDetection(pattern)
+fillUserEmail.writeJsonFile()
 
 
 
-emailContent = ""
-with open('email_template.txt', newline='') as templateFile :
-    emailContent = templateFile.read()
 
-subject = re.search(r'Subject: (.*)\n', emailContent,re.IGNORECASE )
-emailBody = re.search(r'Subject: .*\n\n((.|\n)*)', emailContent,re.IGNORECASE )
-emailInput['Subject'] = subject.group(1)
-# print(subject.group(1))
-# print(emailBody.group(1))
-emailInput['body'] = emailBody.group(1)
-
-
-
-errorFlag = 0
-patternString = input("Enter replace pattern: ")
-pattern = re.search(r'([^a-zA-Z0-9]*)[a-zA-Z\s]+(.*)', patternString )
-while pattern == None or pattern.group(1) == None or pattern.group(1) == '' or pattern.group(2) == None or pattern.group(2) == '':
-    patternString = input("Enter replace pattern again: ")
-    pattern = re.search(r'([^a-zA-Z0-9]*)[a-zA-Z\s]+(.*)', patternString , re.IGNORECASE)
-
-# print(pattern.group(1))
-# print("Div")
-# print(pattern.group(2))
-
-# Could be made more efficient = look for sub with dict search using re.compile
-replacedOutput = emailInput['body']
-# print(replacedOutput)
-print(noOfColumns)
-for i in range(noOfColumns):
-    # print(userData[0][list(userData[0].keys())[i]])
-    # print("" + pattern.group(1) + list(userData[0].keys())[i] + pattern.group(2))
-    if '[' in pattern.group(1) and ']' in pattern.group(2) :
-        patternPrefix = re.sub(r'\[', "\\[", pattern.group(1))
-        patternPostfix = re.sub(r'\]', "\\]", pattern.group(2))
-    # print(patternPrefix)
-    # print(patternPostfix)
-    # print(r'' + patternPrefix + list(userData[0].keys())[i] + patternPostfix)
-    replacedOutput = re.sub(r'' + patternPrefix + list(userData[0].keys())[i] + patternPostfix, userData[0][list(userData[0].keys())[i]] , replacedOutput , flags=re.IGNORECASE ) 
-    # print(replacedOutput)
-
-# print(replacedOutput)
-emailInput['body'] = replacedOutput
-# print(replacedOutput)
-
-for i in range(noOfColumns):
-    print(list(userData[0].keys())[i])
-    # print(emailInput['body'])
-    searchResult = re.search('([^\sa-zA-Z]+)' + list(userData[0].keys())[i] + '([^\sa-zA-Z]+)', emailInput['body'], re.IGNORECASE)
-    # if (searchResult.group(1) != pattern.group(1) and searchResult.group(1) in pattern.group(1)) or (searchResult.group(2) != pattern.group(2) and searchResult.group(2) in pattern.group(2))  :
-    #     errorFlag = 1
-    # print(searchResult.group(1))
-    # print(searchResult.group(2))
-
-    if searchResult ==  None:
-        continue 
-
-    if searchResult.group(1) != pattern.group(1) or searchResult.group(2) != pattern.group(2) :
-        errorFlag = 1
-
-if errorFlag == 1 :
-    print("There are errors in the drafted Email")
-else:
-    print("The drafted email is error free")
-
-jsonOutput = json.dumps(emailInput)
-textInputFile = open('email_formatted.json', 'r+')   
-json.dump(jsonOutput,textInputFile)
-textInputFile.close()
 
 
     
